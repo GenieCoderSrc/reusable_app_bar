b851bf09e0bbeb22c4a8f0a15f2216cebabcda5a
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-08-03 22:11:20 +0600

    Added two-way sync script

--- lib/views/widgets/app_bar_pop_up_menu.dart
+++ lib/views/widgets/app_bar_pop_up_menu.dart
@@ -4,24 +4,29 @@ class AppPopupMenuButton extends StatelessWidget {
   final List<OptionModel> options;
   final ValueChanged<String> onSelected;
 
-  const AppPopupMenuButton(
-      {super.key, required this.options, required this.onSelected});
+  const AppPopupMenuButton({
+    super.key,
+    required this.options,
+    required this.onSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<String>(
       onSelected: onSelected,
-      itemBuilder: (BuildContext context) => options
-          .map(
-            (option) => PopupMenuItem<String>(
-              value: option.name,
-              child: ListTile(
-                leading: Icon(option.icon),
-                title: Text(option.name),
-              ),
-            ),
-          )
-          .toList(),
+      itemBuilder:
+          (BuildContext context) =>
+              options
+                  .map(
+                    (option) => PopupMenuItem<String>(
+                      value: option.name,
+                      child: ListTile(
+                        leading: Icon(option.icon),
+                        title: Text(option.name),
+                      ),
+                    ),
+                  )
+                  .toList(),
     );
   }
 }
